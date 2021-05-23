d5d7c40be105f91d21e93878931f25bfc7d11bbd
Will Larry <will23larry@gmail.com>
2021-05-23 17:13:57 -0400

    Setup nav config in App.js. Add completed login and registration screens. Add placeholder for home screen.

--- App.js
+++ App.js
@@ -18,25 +18,29 @@ const Stack = createStackNavigator();
 export default function App() {
   const [user, setUser] = useState(true);
 
+import 'react-native-gesture-handler';
+import React, {useState} from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {HomeScreen, LoginScreen, RegistrationScreen} from './src/screens';
+import {decode, encode} from 'base-64';
+if (!global.btoa) {
+  global.btoa = encode;
+}
+if (!global.atob) {
+  global.atob = decode;
+}
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  const [user, setUser] = useState(true);
+
   return (
     <NavigationContainer>
       <Stack.Navigator>
         {user ? (
-          <Stack.Screen
-            name="Home"
-            options={{
-              title: 'Splash',
-              headerStyle: {
-                backgroundColor: '#092235',
-              },
-              headerTintColor: '#fff',
-              headerTitleStyle: {
-                fontWeight: 'bold',
-                fontSize: 30,
-                paddingBottom: 5,
-              },
-              headerRight: () => <HeaderIcons />,
-            }}>
+          <Stack.Screen name="Home">
             {props => <HomeScreen {...props} extraData={user} />}
           </Stack.Screen>
         ) : (
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,37 +1,8 @@
-import React, {useEffect, useState} from 'react';
-import {FlatList, StyleSheet} from 'react-native';
-
-import VerticalImageIndex from '../components/VerticalImageIndex';
+import React from 'react';
+import {Text} from 'react-native';
 
 const HomeScreen = () => {
-  const [photos, setPhotos] = useState(null);
-
-  useEffect(() => {
-    fetch(
-      'https://api.unsplash.com/photos/?client_id=Pfsu-9Fcu8eaLxSJSYQlXmxoaS3X90KHGJ8AlSJndnE',
-    )
-      .then(response => response.json())
-      .then(data => {
-        setPhotos(data);
-      });
-  }, []);
-
-  return (
-    <FlatList
-      data={photos}
-      style={styles.flatList}
-      renderItem={({item}) => {
-        return <VerticalImageIndex photo={item} />;
-      }}
-    />
-  );
+  return <Text>Hi, Welcome Home</Text>;
 };
-
-const styles = StyleSheet.create({
-  flatList: {
-    display: 'flex',
-    backgroundColor: '#092235',
-  },
-});
 
 export default HomeScreen;
